6692496b3069f33188eb805e9638aaee4de920ff
kasey <90230268+atekwana@users.noreply.github.com>
2022-12-04 22:55:59 -0500

    Add files via upload

---
+++ classworks/CMSC140_Classwork2/KAtekwana_hw2.cpp
@@ -0,0 +1,11 @@
+/* kasey atekwana
+   february 4, 2021
+   classwork 2
+*/
+
+#include <iostream>
+
+    int main (){
+        std::cout << "kasey atekwana\n3506 hubbard ln\nhyattsville\nmaryland\n20786\n202-553-6308\ncomputer science\n";
+    return 0;
+    }
